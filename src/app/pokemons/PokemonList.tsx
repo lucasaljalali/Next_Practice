c3e631914ff11b79a28396bd9df69c6cd41d4aab
Lucas al Jalali <107360413+lucasaljalali@users.noreply.github.com>
2023-05-01 00:32:25 +0100

    ADJUST: pokemon name

--- src/app/pokemons/PokemonList.tsx
+++ src/app/pokemons/PokemonList.tsx
@@ -5,6 +5,7 @@ export default function PokemonList() {
   const [pokemonListData, setPokemonListData] = useState<IPokemon[] | null>(null);
   const [selectedPokemon, setSelectedPokemon] = useState<string | null>(null);
   const [pokemonCardData, setPokemonCardData] = useState<IPokemon | null>(null);
+  const firstLetterUpper = (name: string) => name.charAt(0).toUpperCase() + name.slice(1);
 
   useEffect(() => {
     async function fetchListData() {
@@ -36,13 +37,16 @@ export default function PokemonList() {
             onClick={() => setSelectedPokemon(pokemon?.name)}
             className="bg-stone-500 hover:bg-stone-600 p-2 rounded-xl flex item-align justify-center text-sm md:text-base cursor-pointer"
           >
-            <span>{pokemon?.name}</span>
+            <span>{firstLetterUpper(pokemon?.name)}</span>
           </div>
         ))}
       </div>
       {pokemonCardData && (
-        <div className="w-3/4 md:w-1/2 bg-stone-500/30 rounded-full">
+        <div className="w-3/4 md:w-1/2 bg-stone-500/30 rounded-full relative">
           <img src={pokemonCardData?.sprites?.front_default} alt="pokemon image" className="w-full" />
+          <span className="absolute left-1/2 bottom-0 -translate-x-1/2 -translate-y-1/2 text-stone-500">
+            {selectedPokemon && firstLetterUpper(selectedPokemon)}
+          </span>
         </div>
       )}
     </div>
