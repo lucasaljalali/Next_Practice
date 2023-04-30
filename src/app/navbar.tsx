6e9476c56f44c7079a6e10091e1645fd2c13bda5
Lucas al Jalali <107360413+lucasaljalali@users.noreply.github.com>
2023-05-01 00:50:00 +0100

    ADJUST: navbar key missing

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -4,9 +4,10 @@ export default function Navbar() {
   const pagesLinks = ["Home", "Pokemons", "Contact"];
   return (
     <div className="flex items-center gap-5 p-6 bg-stone-700 text-md text-stone-300 justify-center sm:justify-start	">
-      {pagesLinks.map((link) => {
+      {pagesLinks?.map((link, index) => {
         return (
           <Link
+            key={index}
             href={link === "Home" ? "/" : `/${link.toLowerCase()}`}
             className="px-4 py-2 text-sm rounded-xl transition-all bg-stone-800 hover:bg-stone-900 sm:px-6 text-md"
           >
